51e16edf85d275b331af1c2fe10a7c85ddbe83f5
Sajith Sasidharan <sajith@nonzen.in>
2015-01-30 16:29:14 -0500

    Add solution to problem 14.

---
+++ 11-20-lists/14.hs
@@ -0,0 +1,17 @@
+{--
+
+(*) Duplicate the elements of a list.
+
+Example:
+
+* (dupli '(a b c c d))
+(A A B B C C C C D D)
+
+Example in Haskell:
+
+> dupli [1, 2, 3]
+[1,1,2,2,3,3]
+
+--}
+
+dupli = concatMap (take 2 . repeat x)
