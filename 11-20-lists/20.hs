8f0b5947885c8926cb2704a0e43a1354f7406c12
Sajith Sasidharan <sajith@nonzen.in>
2015-02-02 17:29:53 -0500

    Add solution to problem 20.

---
+++ 11-20-lists/20.hs
@@ -0,0 +1,32 @@
+{--
+
+(*) Remove the K'th element from a list.
+
+Example in Prolog:
+
+?- remove_at(X,[a,b,c,d],2,R).
+X = b
+R = [a,c,d]
+
+Example in Lisp:
+
+* (remove-at '(a b c d) 2)
+(A C D)
+
+(Note that this only returns the residue list, while the Prolog
+version also returns the deleted element.)
+
+Example in Haskell:
+
+*Main> removeAt 2 "abcd"
+('b',"acd")
+
+--}
+
+-- TODO: Too much indexing.  Rewrite this.
+removeAt :: Int -> [a] -> (a, [a])
+removeAt n xs = (xs !! (n-1), rest n xs)
+  where
+    rest n xs  = get [0..(n-2)] xs ++ get [n..((length xs)-1)] xs
+    get  ns xs = map (\n -> xs !! n) ns
+
