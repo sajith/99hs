c9ad2ba3b3cd40b727372091c857a1a1e8f5034a
Sajith Sasidharan <sajith@nonzen.in>
2015-02-09 11:16:27 -0500

    Add solution for problem 32.

---
+++ 31-41-arithmetic/32.hs
@@ -0,0 +1,24 @@
+{--
+
+(**) Determine the greatest common divisor of two positive integer
+numbers. Use Euclid's algorithm.
+
+http://en.wikipedia.org/wiki/Euclidean_algorithm
+
+Example:
+
+* (gcd 36 63)
+9
+
+Example in Haskell:
+
+[myGCD 36 63, myGCD (-3) (-6), myGCD (-3) 6]
+[9,3,3]
+
+--}
+
+myGCD a b | b == 0    = a
+          | a < 0     = myGCD ((-1) * a) b
+          | b < 0     = myGCD a ((-1) * b)
+          | otherwise = myGCD b (a `mod` b)
+
