3cc66c16271d6fc5594e549ee3e99a2a24413416
Sajith Sasidharan <sajith@nonzen.in>
2015-02-09 18:12:29 -0500

    Add solution to problem 47.
    Not quite sure if I'm done though.

--- 46-50-logic-and-codes/47.hs
+++ 46-50-logic-and-codes/47.hs
@@ -25,4 +25,46 @@ False False False
 
 --}
 
+import           Control.Monad (forM_)
+import           Prelude       hiding (and, or)
+
+------------------------------------------------------------
+
+-- TODO: Actually figure out precedence rules for Java, as required by
+-- the problem statement.
+--
+-- Haskell fixity declaration levels range from 0 to 9, 9 being the
+--strongest.
+
+infixr 9 `and'`
+infixr 8 `or'`
+infixr 7 `impl`
+infixr 6 `equ`
+
+and' True True = True
+and' _  _      = False
+
+or' False False = False
+or' _ _         = True
+
+nor False False = True
+nor _ _         = False
+
+xor True False = True
+xor False True = True
+xor _ _        = False
+
+impl True False = False
+impl _ _        = True
+
+equ = undefined
+
+------------------------------------------------------------
+
+table2 expr = forM_ pairs (eval expr)
+
+eval expr (a,b) = putStrLn $ show a ++ " " ++ show b ++ " " ++ show (expr a b)
+pairs = [(True, True), (True,False), (False,True), (False,False)]
+
+------------------------------------------------------------
 
