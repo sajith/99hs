23fd7c0b6d7f7c58ca784c39dfb758b40a5d3dee
Sajith Sasidharan <sajith@nonzen.in>
2015-02-11 10:06:51 -0500

    Add type signature for leaf.

--- 54a-to-60-binary-trees/BinaryTree.hs
+++ 54a-to-60-binary-trees/BinaryTree.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE RankNTypes #-}
+
 module BinaryTree where
 
 data Tree a = Empty | Branch a (Tree a) (Tree a)
             deriving (Show, Eq)
 
+leaf :: forall a. a -> Tree a
 leaf a = Branch a Empty Empty
 
