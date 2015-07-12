fb400cee9add4fc41058cc76eb97fefa0b4ee3f0
Sajith Sasidharan <sajith@nonzen.in>
2015-07-12 13:33:52 -0400

    Add problems 90-94.

---
+++ 90-94-miscellaneous/94.hs
@@ -0,0 +1,18 @@
+{--
+
+Problem 94
+
+(***) Generate K-regular simple graphs with N nodes
+
+In a K-regular graph all nodes have a degree of K; i.e. the number of
+edges incident in each node is K. How many (non-isomorphic!) 3-regular
+graphs with 6 nodes are there?
+
+Sample results
+
+Example in Haskell:
+
+length $ regular 6 3
+2
+
+--}
